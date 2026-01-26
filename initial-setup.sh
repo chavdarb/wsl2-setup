d89186692931e64c7c5f73cbad054bbf0b9ccfb3
Chavdar Baykov <chavdar.baykov@paysafe.com>
2026-01-26 20:46:19 +0200

    Update the install script

--- initial-setup.sh
+++ initial-setup.sh
@@ -35,5 +35,37 @@ source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk install maven
 
+# Configure Terraform to use a single directory for all plugins/providers
+mkdir -p "${HOME}/.terraform.d/plugin-cache"
+cat <<EOF_terraform_config > "${HOME}/.terraformrc"
+plugin_cache_dir   = "$HOME/.terraform.d/plugin-cache"
+disable_checkpoint = true
+EOF_terraform_config
+
+# kubectl -https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html rm -r ~/.kube
+KUBECTL_VERSION="$(curl -L -s https://dl.k8s.io/release/stable.txt)"
+mkdir -p "${EXECUTION_DIR}"
+pushd /tmp > /dev/null 2>&1 || exit
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+install -v kubectl "${EXECUTION_DIR}/"
+rm kubectl
+popd > /dev/null 2>&1 || exit
+
+# Kubernetes CLI - https://github.com/derailed/k9s
+curl -sS https://webinstall.dev/k9s | bash
+
+# helm - https://helm.sh/docs/intro/install/
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm plugin install https://github.com/belitre/helm-push-artifactory-plugin --version v1.0.2
+helm repo add gitlab "https://charts.gitlab.io"
+helm repo add eks "https://aws.github.io/eks-charts"
+
+# Crane
+# for additional information check: https://github.com/google/go-containerregistry/tree/main/cmd/crane
+CRANE_VERSION="$(curl -s "https://api.github.com/repos/google/go-containerregistry/releases/latest" | jq -r '.tag_name')"
+curl -sL "https://github.com/google/go-containerregistry/releases/download/${CRANE_VERSION}/go-containerregistry_Linux_x86_64.tar.gz" -o go-containerregistry.tar.gz
+tar -zxvf go-containerregistry.tar.gz -C "${EXECUTION_DIR}" crane
+rm go-containerregistry.tar.gz
+
 sudo apt autoremove -y
 echo "Initial setup completed. Please restart wsl using wsl --shutdown and then start your terminal again."
