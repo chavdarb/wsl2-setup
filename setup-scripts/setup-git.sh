c6a94add50180b61821cdd3dcaef1f2315353a54
chavdarbaykov <chavdar.baykov@paysafe.com>
2026-01-26 16:42:14 +0000

    Make username configurable

--- setup-scripts/setup-git.sh
+++ setup-scripts/setup-git.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Enter your Git email:"
+read -r GIT_EMAIL
+echo "Enter your Git username:"
+read -r GIT_USERNAME
+echo ""
+
 echo "Generating SSH key"
-ssh-keygen -o -t rsa -C "chavdar.baykov@paysafe.com"
+ssh-keygen -o -t rsa -C "$GIT_EMAIL"
 echo ""
 echo "Your SSH public key (add this to GitLab):"
 cat ~/.ssh/id_rsa.pub
@@ -10,5 +16,5 @@ read -p "Press any key to continue..." -n1 -s
 echo ""
 
 echo "Configuring Git"
-git config --global user.email "chavdar.baykov@paysafe.com"
-git config --global user.name "chavdarbaykov"
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
